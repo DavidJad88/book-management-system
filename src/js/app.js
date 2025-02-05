f0920aaf3dc74f397bcd763eaaa82cea63e3abf1
David Jadlowkier <davidjadlowkier@Caroline-sin-MacBook-Pro.local>
2025-02-05 13:25:15 +0100

    add toggle function on form

---
+++ src/js/app.js
@@ -0,0 +1,54 @@
+import UserInterface from "./userInterface";
+// SELECTING DOM ELEMENTS
+
+const openAddModalButton = document.querySelector(".add-books__button");
+const closeAddModalButton = document.querySelector(".form__cancel-button");
+const formModal = document.querySelector(".form-modal");
+const printedBookContainer = document.querySelector(".form__printed-book");
+const audioBookContainer = document.querySelector(".form__audio-book");
+
+// SELECTING FORM INPUTS
+
+const form = document.querySelector(".form");
+const title = document.querySelector(".form__title-input");
+const author = document.querySelector(".form__author-input");
+const publisher = document.querySelector(".form__publisher-input");
+const date = document.querySelector(".form__publication-date-input");
+const bookTypeDropdown = document.querySelector(".form__book-type");
+
+const filterContainer = document.querySelector(".filter-books");
+const formSubmitButton = document.querySelector(".form__add-button");
+
+// SELECTING ELEMENTS SPECIFIC TO PRINTED BOOKS
+
+const pages = document.querySelector(".form__pages-input");
+const printType = document.querySelector(".form__print-type");
+
+// SELECTING ELEMENTS SPECIFIC TO AUDIO BOOKS
+
+const narrator = document.querySelector(".form__narrator-input");
+const duration = document.querySelector(".form__duration-input");
+
+// ALL ELEMENTS IN PRINTED AND AUDIO CATEGPORY
+
+const printedFields = [
+  document.querySelector(".form__pages-input"),
+  document.querySelector(".form__print-type"),
+];
+
+const audioFields = [
+  document.querySelector(".form__narrator-input"),
+  document.querySelector(".form__duration-input"),
+];
+
+// ADDING EVENT LISTENERS
+
+bookTypeDropdown.addEventListener("change", () => {
+  UserInterface.toggleBookTypeFields(
+    printedBookContainer,
+    audioBookContainer,
+    audioFields,
+    printedFields,
+    bookTypeDropdown.value
+  );
+});
---
+++ src/js/userInterface.js
@@ -0,0 +1,27 @@
+class UserInterface {
+  static toggleBookTypeFields(
+    printedBookContainer,
+    audioBookContainer,
+    audioFields,
+    printedFields,
+    bookType
+  ) {
+    console.log("test");
+
+    // HIDE BOTH CONTAINERS INIT
+    printedBookContainer.style.display = "none";
+    audioBookContainer.style.display = "none";
+    // RESET OF VALUES
+    printedFields.forEach((field) => (field.value = ""));
+    audioFields.forEach((field) => (field.value = ""));
+
+    // DISPLAYING RELEVANT CONTAINER ON SELECTION
+    if (bookType === "printed-book") {
+      printedBookContainer.style.display = "block";
+    } else {
+      audioBookContainer.style.display = "block";
+    }
+  }
+}
+
+export default UserInterface;
